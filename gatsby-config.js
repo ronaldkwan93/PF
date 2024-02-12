e1c63897154b4c2d05b04afa899add78920abd3f
RonaldKwann <14719@coderacademy.edu.au>
2024-02-12 18:50:45 +1100

    Contact section change

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
           siteName: "Ronald Kwan", // Used in manifest.json
           shortName: "Portfolio", // Used in manifest.json
           startUrl: "/", // Used in manifest.json
-          backgroundColor: "#FFFFFF", // Used in manifest.json
+          backgroundColor: "#000000", // Used in manifest.json
           themeColor: "#000000", // Used in manifest.json
           display: "minimal-ui", // Used in manifest.json
         },
