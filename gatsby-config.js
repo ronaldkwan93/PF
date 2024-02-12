4a91aa5a050c98999fd480dcf0f3167e6e2d94aa
RonaldKwann <14719@coderacademy.edu.au>
2024-02-12 18:22:04 +1100

    changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,10 +7,10 @@ module.exports = {
         manifestSettings: {
           favicon: "./content/images/favicon.png", // Path is relative to the root
           siteName: "Ronald Kwan", // Used in manifest.json
-          shortName: "Ronald Kwan", // Used in manifest.json
+          shortName: "Portfolio", // Used in manifest.json
           startUrl: "/", // Used in manifest.json
-          backgroundColor: "#FF0000", // Used in manifest.json
-          themeColor: "#FFFFFF", // Used in manifest.json
+          backgroundColor: "#FFFFFF", // Used in manifest.json
+          themeColor: "#000000", // Used in manifest.json
           display: "minimal-ui", // Used in manifest.json
         },
         contentDirectory: "./content",
